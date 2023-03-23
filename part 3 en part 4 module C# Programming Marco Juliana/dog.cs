23d16bae608c67706e67154a68d0d6768b2888be
marcosmug <94381046+marcosmug@users.noreply.github.com>
2023-03-23 23:02:33 +0100

    update dog

--- part 3 en part 4 module C# Programming Marco Juliana/dog.cs
+++ part 3 en part 4 module C# Programming Marco Juliana/dog.cs
@@ -1,11 +1,19 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Class1
+namespace part_3_en_part_4_module_C__Programming_Marco_Juliana
 {
-	public dog()
-	{
+    internal class dog
+    {
         private string name;
         private string breed;
         private int age;
+        public dog() { }
+        
+
     }
 }
+
